648f20a5608690fac9b5720b844394d59f94b675
KJSoo95 <wltn7star@naver.com>
2025-05-14 21:27:50 +0900

    ✨ feat : updateTodo 기능 추가 및 필터링 추가가

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -5,9 +5,14 @@ import { TodoListType, TodoType } from "@/types/todo.type";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
+type CompletedType = "all" | "completed" | "incompleted";
+
 const TodoList = () => {
   const queryClient = useQueryClient();
   const [inputValue, setInputValue] = useState("");
+  const [editId, setEditId] = useState("");
+  const [editTitle, setEditTitle] = useState("");
+  const [completed, setCompleted] = useState<CompletedType>("all");
 
   const { data, isFetching, isError } = useQuery({
     queryKey: ["todos"],
@@ -25,6 +30,19 @@ const TodoList = () => {
     await api.delete("/todos/" + targetId);
   };
 
+  type updateType = {
+    editId: string;
+    editTitle?: string;
+    editCompleted?: boolean;
+  };
+
+  const updateTodo = async (updateValue: updateType) => {
+    await api.patch(`/todos/${updateValue.editId}`, {
+      title: updateValue.editTitle,
+      completed: updateValue.editCompleted,
+    });
+  };
+
   const { mutate: addMutate } = useMutation({
     mutationFn: addTodo,
     onSuccess: () => {
@@ -34,6 +52,13 @@ const TodoList = () => {
 
   const { mutate: removeMutate } = useMutation({
     mutationFn: removeTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  const { mutate: updateMutate } = useMutation({
+    mutationFn: updateTodo,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["todos"] });
     },
@@ -53,9 +78,34 @@ const TodoList = () => {
     removeMutate(targetId);
   };
 
+  const handleUpdateTodo = (updateTodo: updateType) => {
+    const updateValue = {
+      editId: updateTodo.editId,
+      editTitle: updateTodo.editTitle,
+      editCompleted: updateTodo.editCompleted,
+    };
+    updateMutate(updateValue);
+    setInputValue("");
+  };
+
+  const handleResetEdit = () => {
+    setEditId("");
+    setEditTitle("");
+  };
+
   if (isFetching) return <div>Loading...</div>;
   if (isError) return <div>Error occurred</div>;
   if (!data) return <div>No data</div>;
+  const todos = data.filter((item) => {
+    switch (completed) {
+      case "completed":
+        return item.completed;
+      case "incompleted":
+        return !item.completed;
+      default:
+        return true;
+    }
+  });
 
   return (
     <div>
@@ -72,13 +122,59 @@ const TodoList = () => {
         <button onClick={handleAddTodo}>제출</button>
       </div>
       <div>
-        {data.map((item) => {
+        <div>
+          <span>총 {data.length}개</span>
+          <span>완료 {data.filter((item) => item.completed).length}개</span>
+        </div>
+        <div>
+          <span onClick={() => setCompleted("all")}>전체 투두</span>
+          <span onClick={() => setCompleted("completed")}>완료 투두</span>
+          <span onClick={() => setCompleted("incompleted")}>미완료 투두</span>
+        </div>
+        {todos.map((item) => {
           return (
             <div key={item.id}>
-              <input type="checkbox" />
-              {item.title}
+              <input
+                type="checkbox"
+                checked={item.completed}
+                onChange={() =>
+                  handleUpdateTodo({
+                    editId: item.id,
+                    editCompleted: !item.completed,
+                  })
+                }
+              />
+              {editId === item.id ? (
+                <>
+                  <input
+                    type="text"
+                    placeholder="수정할 제목"
+                    className="text-black w-12"
+                    value={editTitle}
+                    onChange={(e) => {
+                      setEditTitle(e.target.value);
+                    }}
+                  />
+                </>
+              ) : (
+                item.title
+              )}
               <button onClick={() => handleRemoveTodo(item.id)}>삭제</button>
-              <button>수정</button>
+              {editId === item.id ? (
+                <>
+                  <button
+                    onClick={() => {
+                      handleUpdateTodo({ editId: item.id, editTitle });
+                      handleResetEdit();
+                    }}
+                  >
+                    수정
+                  </button>
+                  <button onClick={handleResetEdit}>취소</button>
+                </>
+              ) : (
+                <button onClick={() => setEditId(item.id)}>수정</button>
+              )}
             </div>
           );
         })}
