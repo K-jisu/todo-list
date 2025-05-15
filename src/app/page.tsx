6e8c969f3949db383538029b52fccdc354c148eb
K-jisu <wltn7star@naver.com>
2025-05-16 03:34:20 +0900

    ♻️ refactor : getTodos로 공통화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import api from "@/apis/axios/api";
-import { TodoListType } from "@/types/todo.type";
+"use server";
+
 import {
   dehydrate,
   HydrationBoundary,
@@ -8,25 +8,27 @@ import {
 import TodoList from "../components/todo-list";
 import { Suspense } from "react";
 import LoadingPage from "./loading";
+import TodoInput from "@/components/todo-input";
+import { getTodos } from "@/apis/todo.api";
 
 export default async function Home() {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ["todos"],
-    queryFn: async () => {
-      const { data } = await api.get("/todos");
-      return data as TodoListType;
-    },
+    queryFn: getTodos,
   });
 
   return (
     <main className="flex flex-col items-center justify-between pt-20">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<LoadingPage />}>
-          <TodoList />
-        </Suspense>
-      </HydrationBoundary>
+      <div className="w-full max-w-2xl p-6 bg-white text-black shadow-md rounded-lg flex flex-col items-center mx-auto">
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <TodoInput />
+          <Suspense fallback={<LoadingPage />}>
+            <TodoList />
+          </Suspense>
+        </HydrationBoundary>
+      </div>
     </main>
   );
 }
--- src/hooks/queries/useTodosQuery.ts
+++ src/hooks/queries/useTodosQuery.ts
@@ -1,13 +1,9 @@
-import api from "@/apis/axios/api";
-import { TodoListType } from "@/types/todo.type";
+import { getTodos } from "@/apis/todo.api";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const useGetTodos = () => {
   return useSuspenseQuery({
     queryKey: ["todos"],
-    queryFn: async () => {
-      const { data } = await api.get("/todos");
-      return data as TodoListType;
-    },
+    queryFn: getTodos,
   });
 };
