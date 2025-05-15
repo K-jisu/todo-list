be61d4d409de0da972d374ab8ea1bc54b96e47b3
K-jisu <wltn7star@naver.com>
2025-05-15 15:16:45 +0900

    ✨ feat : prefetchQuery 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
-import Link from "next/link";
+import api from "@/apis/axios/api";
+import { TodoListType } from "@/types/todo.type";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import TodoList from "../components/todo-list";
 
-export default function Home() {
+export default async function Home() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const { data } = await api.get("/todos");
+      return data as TodoListType;
+    },
+  });
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href={"/todos"} className="text-2xl font-bold">
-          Todo List
-        </Link>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <TodoList />
+        </HydrationBoundary>
       </main>
     </div>
   );
