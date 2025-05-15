00e60e50b8c3fc2def2287deef49fb9a748796a2
K-jisu <wltn7star@naver.com>
2025-05-15 15:55:34 +0900

    ✨ feat : Loading 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import TodoList from "../components/todo-list";
+import { Suspense } from "react";
+import LoadingPage from "./loading";
 
 export default async function Home() {
   const queryClient = new QueryClient();
@@ -22,7 +24,9 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <HydrationBoundary state={dehydrate(queryClient)}>
-          <TodoList />
+          <Suspense fallback={<LoadingPage />}>
+            <TodoList />
+          </Suspense>
         </HydrationBoundary>
       </main>
     </div>
