52f88f3890cec910b5b47693fd1527fd2ffdbb79
K-jisu <wltn7star@naver.com>
2025-05-15 15:14:41 +0900

    🏷️ type : updateType 추가

--- src/types/todo.type.ts
+++ src/types/todo.type.ts
@@ -3,4 +3,11 @@ export type TodoType = {
   title: string;
   completed: boolean;
 };
+
 export type TodoListType = TodoType[];
+
+export type updateType = {
+  editId: string;
+  editTitle?: string;
+  editCompleted?: boolean;
+};
