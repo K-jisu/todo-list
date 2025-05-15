e71837dc628f65076bfd167da4fbc9fa297adcc0
K-jisu <wltn7star@naver.com>
2025-05-16 03:32:18 +0900

    ♻️ refactor : axios 대신 fetch 사용

--- src/apis/todo.api.ts
+++ src/apis/todo.api.ts
@@ -1,17 +1,63 @@
-import { TodoType, updateType } from "@/types/todo.type";
-import api from "./axios/api";
+import { TodoListType, TodoType, updateType } from "@/types/todo.type";
+
+export const getTodos = async () => {
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/todos", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    throw new Error("데이터를 가져오는 데 실패했습니다.");
+  }
+  const data = await response.json();
+  return data as TodoListType;
+};
 
 export const addTodo = async (newTodo: TodoType) => {
-  await api.post("/todos", newTodo);
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/todos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTodo),
+  });
+  if (!response.ok) {
+    throw new Error("할 일 추가에 실패했습니다.");
+  }
 };
 
 export const removeTodo = async (targetId: string) => {
-  await api.delete("/todos/" + targetId);
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "/todos/" + targetId,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error("할 일 삭제에 실패했습니다.");
+  }
 };
 
 export const updateTodo = async (updateValue: updateType) => {
-  await api.patch(`/todos/${updateValue.editId}`, {
-    title: updateValue.editTitle,
-    completed: updateValue.editCompleted,
-  });
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "/todos/" + updateValue.editId,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: updateValue.editTitle,
+        completed: updateValue.editCompleted,
+      }),
+    }
+  );
+  if (!response.ok) {
+    throw new Error("할 일 수정에 실패했습니다.");
+  }
 };
